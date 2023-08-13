51bce80e53689ce0a4f2e2e08be8dc338cdc1167
Shubh A Chudasama <shubh.cs@proton.me>
2023-08-13 10:10:53 +0530

    Fix title on tracks page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,24 @@
 // Faq
 // Footer
 
-import Head from 'next/head';
-import React from 'react';
-import { FAQ } from '../src/components/FAQ';
-import { Hero } from '../src/components/Hero';
-import { Footer } from '../src/components/Layouts/Footer';
-import { Nav } from '../src/components/Layouts/Nav';
-import { Motto } from '../src/components/Motto';
-import { Testimonial } from '../src/components/Testimonial';
+import Head from "next/head";
+import React from "react";
+import { FAQ } from "../src/components/FAQ";
+import { Hero } from "../src/components/Hero";
+import { Footer } from "../src/components/Layouts/Footer";
+import { Nav } from "../src/components/Layouts/Nav";
+import { Motto } from "../src/components/Motto";
+import { Testimonial } from "../src/components/Testimonial";
 
 const Home: React.FC = () => {
   return (
     <React.Fragment>
       <Head>
-        <title>GDSC</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>GDSC - CVR</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
-      <main className='pt-24'>
+      <main className="pt-24">
         <Hero />
         <Motto />
         <Testimonial />
