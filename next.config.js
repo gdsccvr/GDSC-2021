ec11e6f91b99e6b96b7f6037b7f70e51fb18bc13
Shubh A Chudasama <shubh.cs@proton.me>
2024-01-26 11:23:40 +0530

    Add bitnbuild redirects

--- next.config.js
+++ next.config.js
@@ -3,9 +3,30 @@ module.exports = {
   webpack: (cfg) => {
     cfg.module.rules.push({
       test: /\.md$/,
-      loader: 'frontmatter-markdown-loader',
-      options: { mode: ['react-component'] },
+      loader: "frontmatter-markdown-loader",
+      options: { mode: ["react-component"] },
     });
     return cfg;
+  },
+  async redirects() {
+    return [
+      {
+        source: "/bitnbuild/brochure",
+        destination: "https://online.flippingbook.com/view/981439766/",
+        permanent: false,
+      },
+      /* TODO */
+      // {
+      //   source: "/bitnbuild/registration",
+      //   destination: "",
+      //   permanent: false,
+      // },
+      {
+        source: "/bitnbuild/problem-statements",
+        destination:
+          "https://potent-hyssop-b42.notion.site/Internal-Problem-Statements-801ed1518faf4122bb8beec2a390cc46",
+        permanent: false,
+      },
+    ];
   },
 };
